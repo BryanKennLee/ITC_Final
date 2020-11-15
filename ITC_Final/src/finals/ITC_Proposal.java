305c916fa9efe2c194f482ceed605c3f82d706e2
Lee Kenn <Lee Kenn@DESKTOP-DJ28EV1>
2020-11-15 18:19:36 +0800

    Second Commit

--- ITC_Final/src/finals/ITC_Proposal.java
+++ ITC_Final/src/finals/ITC_Proposal.java
@@ -2,7 +2,16 @@ package finals;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Font;
+import java.awt.Color;
+import java.util.*;
+import java.text.*;
+
+import java.sql.*;
+import net.proteanit.sql.DbUtils;
 
 public class ITC_Proposal {
 
@@ -36,8 +45,46 @@ public class ITC_Proposal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 312, 441);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton AddManga_Button = new JButton("Add Manga");
+		AddManga_Button.setBackground(Color.BLACK);
+		AddManga_Button.setFont(new Font("Tahoma", Font.PLAIN, 27));
+		AddManga_Button.setBounds(44, 175, 217, 76);
+		frame.getContentPane().add(AddManga_Button);
+		
+		JLabel Title_Label = new JLabel("Mangalot");
+		Title_Label.setFont(new Font("Tahoma", Font.PLAIN, 49));
+		Title_Label.setBounds(44, 40, 217, 76);
+		frame.getContentPane().add(Title_Label);
+		
+		JButton ViewManga_Button = new JButton("View Manga");
+		ViewManga_Button.setBackground(Color.BLACK);
+		ViewManga_Button.setFont(new Font("Tahoma", Font.PLAIN, 27));
+		ViewManga_Button.setBounds(44, 277, 217, 76);
+		frame.getContentPane().add(ViewManga_Button);
 	}
-
+	
+	/**
+	 * 
+	 * Connect the database to the GUI
+	 */
+	public static Connection connect() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+	        //System.out.println("Loaded driver");
+	        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/mysql?user=root&password=edureka");
+	        //System.out.println("Connected to MySQL");
+	        return con;
+		} catch(Exception ex){
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	
+	
 }
